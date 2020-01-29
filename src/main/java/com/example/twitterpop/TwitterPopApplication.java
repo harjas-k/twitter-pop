6f15b0e4d0e8ea96bce9d3d1494e19afe576e263
hk671810 <harjas.kaur@broadcom.com>
2020-01-28 20:34:24 -0600

    initial commit

---
+++ src/main/java/com/example/twitterpop/TwitterPopApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitterpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterPopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterPopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/twitterpop/TwitterPopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitterpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterPopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
