4fd1384f8248a4fa96f16bc620f2f8af9a1e74f4
hk671810 <harjas.kaur@broadcom.com>
2020-01-28 21:03:45 -0600

    added app controller

---
+++ src/main/java/com/example/twitterpop/AppController.java
@@ -0,0 +1,12 @@
+package com.example.twitterpop;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @RequestMapping("/")
+    public String index(){
+        return "Greetings!";
+    }
+}
